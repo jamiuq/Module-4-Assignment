66dbc7b25d11d9da8f7de5b8db2b77463e0900ab
jamiuq <jamiuq@farmingdale.edu>
2025-02-19 03:28:42 +0000

    did questions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+/*
+1. I couldn't find the video anywhere on lynda.com.
+Peggy Fisher did have an essential training course but it was for COBOL
 
-// import org.junit.jupiter.api.Test;
+2. Encapsulation is the grouping of data into a single unit, and also keeping it hidden from the user.
+
+3. A null reference is when a variable doesn't point to any memory. It is mostly redundant
+
+4. Inheritance is when a class inherits the properties of another class. Composition is when a class has a reference to another class. Inheritance is an Is-A relationship, Composition is a Has-A relationship.
+
+*/
 
 public class Main {
   public static void main(String[] args) 
