eedec774ad45b022b341a86982d91ca1bebc1824
jamiuq <jamiuq@farmingdale.edu>
2025-02-19 03:34:36 +0000

    modified

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,16 +20,28 @@ public class Main {
     arr[2] = new sportsCar(1995, "Mazda", "John Doe");
     arr[3] = new junkCar(1996, "Ford", "Used");
 
+    
+
+    
     for (Automobile a : arr)
     {
       a.setSpeed((int)(Math.random() * 151));
     }
 
+    arr[0].brake();
+    arr[1].reverse();
+    arr[2].accelerate();
+    arr[3].honk();
+    
     int fastestCar = 0;
+    
+    System.out.println("\n" + arr[0]);
+    
     for (int i = 1; i < arr.length; i++)
     {
+      System.out.println(arr[i]);
       if (arr[i].compareTo(arr[fastestCar]) > 0) fastestCar = i;
     }
-    System.out.println("Fastest car:\n" + arr[fastestCar]);
+    System.out.println("\nFastest car:\n" + arr[fastestCar]);
   }
 }
