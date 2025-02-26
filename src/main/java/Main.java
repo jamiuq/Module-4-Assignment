6500501478ffa785060d2f07e13ab2307a3c365f
jamiuq <jamiuq@farmingdale.edu>
2025-02-26 06:11:32 +0000

    ..

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,35 +13,6 @@ Peggy Fisher did have an essential training course but it was for COBOL
 public class Main {
   public static void main(String[] args) 
   {
-    Automobile[] arr = new Automobile[4];
-
-    arr[0] = new Automobile(1990, "Toyota");
-    arr[1] = new Car(1994, "Honda", "ABC123");
-    arr[2] = new sportsCar(1995, "Mazda", "John Doe");
-    arr[3] = new junkCar(1996, "Ford", "Used");
-
     
-
-    
-    for (Automobile a : arr)
-    {
-      a.setSpeed((int)(Math.random() * 151));
-    }
-
-    arr[0].brake();
-    arr[1].reverse();
-    arr[2].accelerate();
-    arr[3].honk();
-    
-    int fastestCar = 0;
-    
-    System.out.println("\n" + arr[0]);
-    
-    for (int i = 1; i < arr.length; i++)
-    {
-      System.out.println(arr[i]);
-      if (arr[i].compareTo(arr[fastestCar]) > 0) fastestCar = i;
-    }
-    System.out.println("\nFastest car:\n" + arr[fastestCar]);
   }
 }
